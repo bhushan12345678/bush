699b91886b27afa472327c6589c40e71923e98fe
bhushan12345678 <39933017+bhushan12345678@users.noreply.github.com>
2021-05-21 13:38:49 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,63 @@
+package gchgcchgc;
+
+
+
+
+
+
+public class number {
+	
+	private int num;
+	private String s;
+	private char k;
+	
+	
+	
+	number(int num, String s,char k){
+		this.num=num;
+		this.s=s;
+		this.k=k;
+	}
+
+
+
+	public int getNum() {
+		return num;
+	}
+
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+
+
+	public String getS() {
+		return s;
+	}
+
+
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+
+
+	public char getK() {
+		return k;
+	}
+
+
+
+	public void setK(char k) {
+		this.k = k;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ number2.java
@@ -0,0 +1,23 @@
+package gchgcchgc;
+
+public class number2 extends number {
+
+	number2(int num, String s, char k) {
+		super(num, s, k);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void number() {
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
